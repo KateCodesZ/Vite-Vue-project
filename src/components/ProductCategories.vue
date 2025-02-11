ac52ad7aebacb17ac12807be98c7dbda04ca4039
Ecaterina Zhukouskaya <katjas.emailg@gmail.com>
2025-02-11 19:58:17 +0100

    small styling update

--- src/components/ProductCategories.vue
+++ src/components/ProductCategories.vue
@@ -18,21 +18,27 @@
 </script>
 
 <template>
-  <div class="overflow-x-auto">
-    <div class="grid grid-cols-5 gap-4 min-w-max px-8">
-      <div
-        v-for="category in state.categories"
-        :key="category.name"
-        class="flex flex-col items-center text-center cursor-pointer"
-      >
-        <img
-          :src="category.image"
-          :alt="category.name"
-          class="sm:h-20 w-20 md:h-30 md:w-30 object-contain"
-        />
-        <span class="mt-2 text-xs md:text-sm font-medium">
-          {{ category.name }}
-        </span>
+  <div class="max-w-screen-lg mx-auto">
+    <div class="flex justify-between items-center m-2 md:m-6 px-8 lg:px-12">
+      <h2 class="text-lg md:text-xl font-bold">Produkter</h2>
+      <a href="#" class="text-blue-600 underline">VISA ALLA</a>
+    </div>
+    <div class="overflow-x-auto">
+      <div class="grid grid-cols-5 gap-4 min-w-max px-8">
+        <div
+          v-for="category in state.categories"
+          :key="category.name"
+          class="flex flex-col items-center text-center cursor-pointer"
+        >
+          <img
+            :src="category.image"
+            :alt="category.name"
+            class="sm:h-20 w-20 md:h-30 md:w-30 object-contain"
+          />
+          <span class="mt-2 text-xs md:text-sm font-medium">
+            {{ category.name }}
+          </span>
+        </div>
       </div>
     </div>
   </div>
