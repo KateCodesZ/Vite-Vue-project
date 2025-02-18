093d89ba09eaa23e5d51c957aa2dd716f3d8a7fb
Ecaterina Zhukouskaya <katjas.emailg@gmail.com>
2025-02-18 15:55:48 +0100

    update sale items layout

--- src/components/SaleItems.vue
+++ src/components/SaleItems.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-screen-xl mx-auto">
-    <div class="flex justify-between items-center my-6 md:m-6 px-6 lg:px-12">
+    <div class="flex justify-between items-center my-8 md:m-6 px-6 lg:px-12">
       <h2
         class="text-3xl md:text-4xl lg:text-5xl font-montserrat-alt text-marianblue font-semibold"
       >
@@ -9,7 +9,7 @@
       <a href="#" class="text-gray-600 font-medium underline">VISA ALLA</a>
     </div>
     <!-- Product Cards Container -->
-    <div class="grid grid-cols-2 lg:grid-cols-4 gap-8 px-4">
+    <div class="grid grid-cols-2 lg:grid-cols-4 gap-8 px-4 mb-12 my-10">
       <div v-for="product in saleItems" :key="product.id" class="flex flex-col">
         <!-- Image container with 4:5 ratio -->
         <div class="relative w-full pb-[125%]">
@@ -18,18 +18,11 @@
             :src="product.image"
             :alt="product.title"
           />
-          <div class="absolute top-2 left-2 bg-mahogany text-white text-sm px-2 py-1 rounded">
-            SALE
-          </div>
+          <div class="absolute top-2 left-2 bg-mahogany text-white text-sm px-2 py-1">SALE</div>
         </div>
-        <!-- Product info -->
-        <div class="mt-4">
-          <div class="flex justify-between">
-            <p class="text-base font-medium text-gray-600 truncate mr-2">{{ product.title }}</p>
-            <p class="text-base font-semibold text-gray-600 whitespace-nowrap">
-              {{ product.price }} KR
-            </p>
-          </div>
+        <div class="mt-4 flex flex-col gap-2">
+          <p class="text-base font-semibold text-gray-600 truncate">{{ product.title }}</p>
+          <p class="text-base font-medium text-gray-600">{{ product.price }} KR</p>
         </div>
       </div>
     </div>
